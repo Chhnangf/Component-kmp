608d36e54b18d4ada01a493b23f1438768ebd1bf
Chhnang <1057970659@qq.com>
2024-06-02 16:19:44 +0800

    fix ios run bug

--- composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            //Navigator(MainScreen())
             App()
         }
     }
